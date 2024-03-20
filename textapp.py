df0a5f6841e7ef13cb212be0ae296dc6cfc76499
kyshGulab <122585945+kyshGulab@users.noreply.github.com>
2024-03-19 22:59:54 -0400

    Update textapp.py

--- textapp.py
+++ textapp.py
@@ -2,3 +2,18 @@ import streamlit as st
 import joblib
 
 model = joblib.load('model_pipeline_model.joblib')
+
+def predict_text_with_score(model, text):
+    prediction = model.predict([text])
+    prediction_proba = model.predict_proba([text])
+    class_labels = model.classes_
+    proba_scores = {class_labels[i]: prediction_proba[0][i] for i in range(len(class_labels))}
+    return prediction[0], proba_scores
+
+
+st.title('Text Classification App')
+user_input = st.text_area("Enter text here:")
+if st.button('Predict'):
+    prediction, scores = predict_text_with_score(model, user_input)
+    st.write("Predicted class:", prediction)
+    st.write("Probability Scores:", scores)
